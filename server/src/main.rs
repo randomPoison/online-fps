d00000e7241097d60d71af4a5bb9b29f8825819c
David LeGare <excaliburhissheath@gmail.com>
2017-10-10 23:22:50 -0500

    Do client I/O on a dedicated thread because tokio

--- server/src/main.rs
+++ server/src/main.rs
@@ -9,84 +9,89 @@ extern crate tokio_io;
 use core::{ClientMessage, LineCodec, Player, ReadyIter, ServerMessage};
 use std::io;
 use std::str;
+use std::thread;
 use std::time::*;
 use futures::{future, Future, Stream, Sink};
+use futures::sync::mpsc;
 use futures_cpupool::CpuPool;
 use math::*;
 use tokio_core::net::TcpListener;
-use tokio_core::reactor::{Core, Interval};
+use tokio_core::reactor::Core;
 use tokio_io::AsyncRead;
 
 fn main() {
-    // Create the event loop that will drive this server.
-    let mut core = Core::new().unwrap();
-    let handle = core.handle();
-//    let reactor = CpuPool::new_num_cpus();
+    // Create a channel for sending new clients from the I/O thread to the main game.
+    let (client_sender, client_receiver) = mpsc::unbounded();
 
-    let mut clients = Vec::<Client>::new();
+    // Spawn a thread dedicated to handling all I/O with clients.
+    thread::spawn(move || {
+        // Create the event loop that will drive this server.
+        let mut core = Core::new().unwrap();
+        let handle = core.handle();
 
-    // Bind the server's socket.
-    let addr = "127.0.0.1:1234".parse().unwrap();
-    let mut incoming = TcpListener::bind(&addr, &handle).expect("Failed to bind socket");
+        // Bind the server's socket.
+        let addr = "127.0.0.1:1234".parse().unwrap();
+        let mut incoming = TcpListener::bind(&addr, &handle)
+            .expect("Failed to bind socket")
+            .incoming()
+            .for_each(move |(socket, address)| {
+                println!("Got me a client: {:?}", address);
 
-    // Run the main loop of the game.
-    let frame_time = Duration::from_secs(1) / 60;
-    let delta = 1.0 / 60.0;
-    let main_loop = Interval::new(frame_time, &handle)
-        .expect("Failed to create main loop")
-        .for_each(|_| {
-            while let Ok((stream, address)) = incoming.accept() {
-                println!("Got me a new client, {:?}", address);
+                // Create channels for passing incoming and outgoing messages to and from the main
+                // game.
+                let (incoming_sender, incoming_receiver) = mpsc::unbounded();
+                let (outgoing_sender, outgoing_receiver) = mpsc::unbounded();
 
                 // Convert the codec into a pair stream/sink pair using our codec to
                 // delineate messages.
-                let (sink, stream) = stream.framed(LineCodec).split();
+                let (sink, stream) = socket.framed(LineCodec).split();
 
                 // Automatically perform JSON conversion for incoming/outgoing messages.
                 let stream = stream.map(|message_string| {
                     serde_json::from_str(&*message_string).expect("Failed to deserialize JSON")
                 });
-
                 let mut sink = sink.with(|message: ServerMessage| {
-                    let message_string = serde_json::to_string(&message)
-                        .expect("Failed to serialize to JSON");
-                    Ok(message_string)
+                    serde_json::to_string(&message)
+                        .map_err(|_| io::Error::new(io::ErrorKind::Other, "Shit's fucked"))
                 });
 
-                let player = Player {
-                    position: Point::origin(),
-                    orientation: Orientation::new(),
-                };
-
-                // Send the player's current state to the new client.
-                sink.start_send(ServerMessage::PlayerUpdate(player.clone()))
-                    .expect("Couldn't begin send of message");
-
-                // Save the client so we can send more junk to them.
-                clients.push(Client {
-                    stream: Box::new(stream),
-                    sink: Box::new(sink),
-                    player,
+                // Setup tasks for pumping incoming and outgoing messages.
+                let incoming_task = stream.for_each(move |message: ClientMessage| {
+                    incoming_sender.unbounded_send(message)
+                        .expect("Failed to send incoming message to main game");
+                    Ok(())
+                }).map_err(|error| {
+                    panic!("Error with incoming message: {:?}", error);
                 });
-            }
 
-            // Move each connected player and send the new position to the client.
-            for client in &mut clients {
-                client.player.position += Vector3::new(1.0, 1.0, 1.0) * delta;
+                let outgoing_task = outgoing_receiver.for_each(move |message: ServerMessage| {
+                    sink.start_send(message).expect("Failed to start send on outgoing message");
+                    Ok(())
+                }).map_err(|error| {
+                    panic!("Error with incoming message: {:?}", error);
+                });
 
-                client.sink.start_send(ServerMessage::PlayerUpdate(client.player.clone()))
-                    .expect("Couldn't begin send of message");
-            }
+                // Spawn the tasks onto the reactor.
+                handle.spawn(incoming_task);
+                handle.spawn(outgoing_task);
 
-            // TODO: Flush outgoing messages for the clients.
+                // Send the incoming and outgoing message channels to the main game.
+                client_sender.unbounded_send((outgoing_sender, incoming_receiver))
+                    .expect("Failed to send message channels to main game");
 
-            Ok(())
-        });
-    core.run(main_loop).expect("Error running the main loop");
-}
+                Ok(())
+            });
 
-struct Client {
-    stream: Box<Stream<Item = ClientMessage, Error = io::Error>>,
-    sink: Box<Sink<SinkItem = ServerMessage, SinkError = io::Error>>,
-    player: Player,
+        core.run(incoming).expect("Error handling incoming connections");
+    });
+
+    // Run the main loop of the game.
+    for client in client_receiver.wait() {
+        let (sender, receiver) = client.expect("Error getting client on main thread");
+        println!("Main game got some client channels!");
+        sender.unbounded_send(ServerMessage::PlayerUpdate(Player {
+            position: Point::origin(),
+            orientation: Orientation::new(),
+        })).expect("Failed to send message to client");
+    }
 }
