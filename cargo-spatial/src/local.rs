f96033f4779bbcf75e3cfd68f02cc5bbfef3bfd4
David LeGare <dlegare.1001@gmail.com>
2019-01-31 14:30:43 -0600

    Run code generation as part of cargo-spatial local launch

--- cargo-spatial/src/local.rs
+++ cargo-spatial/src/local.rs
@@ -5,16 +5,44 @@ use tap::*;
 
 /// Builds workers and then runs `spatial local launch`.
 pub fn launch(_opt: &Opt, _local: &Local, launch: &LocalLaunch) {
+    process::Command::new("which")
+        .arg("setup")
+        .status()
+        .unwrap();
+
+    // Run codegen and such.
+    // TODO: Use the spatialos-sdk-tools crate directly rather than invoking the CLI.
+    // TODO: Make the various flags configurable.
+    let status = dbg!(process::Command::new("setup").args(&[
+        "-s",
+        "schema",
+        "-c",
+        "workers/src/generated.rs",
+        "-o",
+        "schema/bin",
+    ]))
+    .status()
+    .expect("Failed to run setup script");
+
+    if !status.success() {
+        return;
+    }
+
     // Use `cargo install` to build workers and copy the exectuables to the build directory.
     if !launch.no_build {
-        process::Command::new("cargo")
+        let status = process::Command::new("cargo")
             .args(&["install", "--root", "./build/debug", "--debug", "--force"])
+            // TODO: These values need to be configurable.
             .arg("--path")
             .arg("./workers")
             .arg("--bin")
             .arg("server")
             .status()
             .expect("Failed to build worker bin");
+
+        if !status.success() {
+            return;
+        }
     }
 
     // Run `spatial alpha local launch` with any user-specified flags.
